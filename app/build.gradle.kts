e5bf743963994aa837cb3f06d792e66b586eb09d
minhtb27112004 <nguyendacminh2711@gmail.com>
2025-03-29 14:41:00 +0700

    update grade

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,11 +3,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.btl_android"
+    namespace = "com.example.duan1"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.example.btl_android"
+        applicationId = "com.example.duan1"
         minSdk = 35
         targetSdk = 35
         versionCode = 1
@@ -26,8 +26,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
 }
 
@@ -40,4 +40,12 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation("com.github.dhaval2404:imagepicker:2.1")
+    implementation ("me.relex:circleindicator:2.1.6")
+
+    implementation ("com.google.android.material:material:<version>")
+
+
+    implementation ("com.github.bumptech.glide:glide:4.15.1");
+
 }
