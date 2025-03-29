6678aba9c3bd8d06f076d3002332c69935df7c57
minhtb27112004 <nguyendacminh2711@gmail.com>
2025-03-29 17:03:24 +0700

    update login sign up

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,12 +12,15 @@ pluginManagement {
     }
 }
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositoriesMode.set(RepositoriesMode.PREFER_PROJECT) // Tránh lỗi khi có repo trong module
     repositories {
         google()
         mavenCentral()
+         jcenter() // Chỉ bật nếu thực sự cần thiết
+        maven("https://jitpack.io")
     }
 }
+
 
 rootProject.name = "BTL_ANDROID"
 include(":app")
